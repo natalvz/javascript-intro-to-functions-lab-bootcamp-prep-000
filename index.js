d20412484bc37e49682dcb2ea4abc3a4ed02b95b
Natalia Castro <nat.alvz@gmail.com>
2018-04-12 16:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string===string.toLowerCase())
+  if(string===string.toLowerCase()){
++      var lowerCase = string.toLowerCase();
++      return ('I can\'t hear you!');
   
 }
 
